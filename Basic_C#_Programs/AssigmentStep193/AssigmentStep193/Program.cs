91c94ac6e76d52bc50029d0cb48fd0362ba03dbb
emmorris93 <maudi.morris@gmail.com>
2021-01-11 19:09:25 -0800

    Added AssignmentStep193
    added Assignment Step 193

--- Basic_C#_Programs/AssigmentStep193/AssigmentStep193/Program.cs
+++ Basic_C#_Programs/AssigmentStep193/AssigmentStep193/Program.cs
@@ -5,10 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 //ASSIGNMENT (Step 193)
-//Create a console app that does/includes the following:
-    //1) Concatenates three strings.
-    //2) Converts a string to uppercase.
-    //3) Creates a Stringbuilder and builds a paragraph of text, one sentence at a time.
+    //Create a console app that does/includes the following:
+        //1) Concatenates three strings.
+        //2) Converts a string to uppercase.
+        //3) Creates a Stringbuilder and builds a paragraph of text, one sentence at a time.
 
 namespace AssigmentStep193
 {
@@ -16,5 +16,21 @@ namespace AssigmentStep193
     {
         static void Main(string[] args)
         {
+            StringBuilder sb = new StringBuilder();
+
+            string a = "Rush: ", b = "The ", c = "Trees";
+            sb.Append(a.ToUpper());
+            sb.Append(b);
+            sb.AppendLine(c);
+            sb.AppendLine();
+
+            sb.AppendLine("There is unrest in the forest");
+            sb.AppendLine("There is trouble with the trees");
+            sb.AppendLine("For the maples want more sunlight");
+            sb.AppendLine("And the oaks ignore their pleas");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
         }
+    }
 }
