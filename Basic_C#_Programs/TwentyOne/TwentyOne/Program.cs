8af3a3f58f0be0d5153eb25fdced2b955514ffb1
emmorris93 <maudi.morris@gmail.com>
2021-01-13 16:34:16 -0800

    Added TwentyOne
    added TwentyOne

---
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// object-oriented programming:
+// Objects are items that can be represented by a computer program.
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
