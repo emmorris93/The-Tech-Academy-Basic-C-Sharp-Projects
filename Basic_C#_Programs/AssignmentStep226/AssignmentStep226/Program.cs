183ca4320c9644d8659f2193159a24edbbcd3932
emmorris93 <maudi.morris@gmail.com>
2021-01-15 15:19:17 -0800

    Created Assignment 226, 227, 228
    created assignments 226, 227, 228

---
+++ Basic_C#_Programs/AssignmentStep226/AssignmentStep226/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//ASSIGNMENT (Step 226)
+//Perform these actions and create a console app that includes the following:
+//1.Create a class. In that class, create a method that will take in an integer, create a math operation for this integer (addition, subtraction, etc.), then return the answer as an integer.
+//2.In the Main() method of the console app, instantiate the class and call the one method, passing in an integer. Display the result to the screen.
+//3. Add a second method to the class with the same name that will take in a decimal, create a different math operation for it, then return the answer as an integer.
+//4. In the Main() method of the console app, instantiate the class and call the second method, passing in a decimal. Display the result to the screen.
+//5. Add a third method to the class, with the same name, that will take in a string, convert it to an integer if possible, do a different math operation to it, then return the answer as an integer.
+//6. In the Main() method of the console app, instantiate the class and call the third method, passing in a string that equates to an integer. Display the result to the screen.
+
+namespace AssignmentStep226
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/AssignmentStep227/AssignmentStep227/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//ASSIGNMENT (Step 227)
+//Perform these actions and create a console app that includes the following:
+//1.Create a class. In that class, create a method that takes two integers as parameters. Make one of them optional. Have the method do a math operation and return an integer result.
+//2. In the Main() method of the console app, instantiate the class.
+//3.Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second number.
+//4. Call the method in the class, passing in the one or two numbers entered.
+//5. Try various combinations of numbers on the code, including having no second number.
+
+namespace AssignmentStep227
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
