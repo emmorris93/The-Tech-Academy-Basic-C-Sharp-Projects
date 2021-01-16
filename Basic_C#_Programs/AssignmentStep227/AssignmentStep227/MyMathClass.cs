7707c9f00cd5304f5a1751629b84a763edbd164a
emmorris93 <maudi.morris@gmail.com>
2021-01-16 14:51:08 -0800

    Added AssignmentStep227
    added assignment step 227

---
+++ Basic_C#_Programs/AssignmentStep227/AssignmentStep227/MyMathClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentStep227
+{
+    public class MyMathClass
+    {
+        public int MultiplicationMethod(int inputOne, int inputTwo = 2)
+        {
+            int multiplyAnswer = inputTwo * inputOne;
+            return multiplyAnswer;
+        }
+    }
+}
--- Basic_C#_Programs/AssignmentStep227/AssignmentStep227/Program.cs
+++ Basic_C#_Programs/AssignmentStep227/AssignmentStep227/Program.cs
@@ -18,7 +18,32 @@ namespace AssignmentStep227
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Input two integers: ");
+            Console.WriteLine("Enter integer One:");
+            int inputOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter integer Two (optional):");
+            string rawInputTwo = Console.ReadLine();
+            int inputTwo = int.MinValue;
+            try
+            {
+                inputTwo = Convert.ToInt32(rawInputTwo);
+            }
 
+            catch
+            {
+                // do nothing
+            }
+            
+
+            MyMathClass myClass = new MyMathClass();
+            if(inputTwo == int.MinValue)
+               Console.WriteLine(myClass.MultiplicationMethod(inputOne));
+            else
+            {
+                Console.WriteLine(myClass.MultiplicationMethod(inputOne, inputTwo));
+            }
+
+            Console.ReadLine();
         }
     }
 }
