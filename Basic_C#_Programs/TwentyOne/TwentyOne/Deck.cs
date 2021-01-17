a7b37d1c4c522f0f7a083204d7f5beb125d68109
emmorris93 <maudi.morris@gmail.com>
2021-01-16 16:21:12 -0800

    Updated TwentyOne
    updated TwentyOne

--- Basic_C#_Programs/TwentyOne/TwentyOne/Deck.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Deck.cs
@@ -6,17 +6,10 @@ using System.Threading.Tasks;
 
 namespace TwentyOne
 {
-    /// <summary>
-    /// 
-    /// </summary>
+
     public class Deck
     {
-        // properties, members
-        public List<Card> Cards { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
+ 
         public Deck()
         { 
             Cards = new List<Card>();
@@ -37,7 +30,28 @@ namespace TwentyOne
                 }
             }
         }
-        
-        //methods
+
+        // properties, members
+        public List<Card> Cards { get; set; }
+
+        public void Shuffle(int times = 1)
+        {
+            
+            for (int i = 0; i < times; i++)
+            {
+                
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+
+                this.Cards = TempList;
+            }
+        }       
     }
 }
