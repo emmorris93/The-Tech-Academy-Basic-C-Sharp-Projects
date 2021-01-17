35ef129de331ba7ce8d3bf6b6e5957093eb7c87f
emmorris93 <maudi.morris@gmail.com>
2021-01-16 17:49:00 -0800

    Updates to TwentyOne, Added AssignmentStep236
    updates to TwentyOne, added assignment step 236

---
+++ Basic_C#_Programs/AssignmentStep236/AssignmentStep236/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentStep236
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            string FullName = FirstName + " " + LastName;
+            Console.WriteLine("Name: " + FullName);
+        }
+    }
+}
--- Basic_C#_Programs/TwentyOne/TwentyOne/Card.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Card.cs
@@ -6,28 +6,18 @@ using System.Threading.Tasks;
 
 namespace TwentyOne
 {
-    /// <summary>
-    /// 
-    /// </summary>
+ 
     public class Card
     {
         public string Suit { get; set; }
         public string Face { get; set; }
 
-        /// <summary>
-        /// 
-        /// </summary>
         public Card() 
         {
             Suit = "Spades";
             Face = "Two";
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="Suit"></param>
-        /// <param name="Face"></param>
         public Card(string Suit, string Face)
         {
             this.Suit = Suit;
