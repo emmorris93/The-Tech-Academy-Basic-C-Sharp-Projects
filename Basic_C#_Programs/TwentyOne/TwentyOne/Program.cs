4de87dc4ebbd38853c88b789c07fa11cda9bdb04
emmorris93 <maudi.morris@gmail.com>
2021-01-31 18:54:42 -0800

    Updated TwentyOne
    updated TwentyOne

--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -10,29 +10,31 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Grand Hotel & Casino.  Let's start by telling me your name:");
+            string playerName = Console.ReadLine();
             
-            Deck deck = new Deck();
-
-
-
-
-            deck.Shuffle(3);
-
-            foreach (Card card in deck.Cards)
+            Console.WriteLine("How much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Hello, {0}.  Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            
+            if(answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
             {
-                Console.WriteLine(card.Face + " of " + card.Suit);
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while(player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
             }
-
-            Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Feel free to look around the casino.  Bye for now.");
+            
             Console.ReadLine();
         }       
     }
 }
-
-//int count = deck.Cards.Count(x => x.Face == Face.Ace);
-
-//List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList();
-
-//List<int> numberList = new List<int> { 1, 2, 3, 5, 7, 18, 29 };
-//int sum = numberList.Sum();
-//Console.WriteLine(sum);
