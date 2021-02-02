9cc3caf9cfe4402c3c972492663f6fcb190456f6
emmorris93 <maudi.morris@gmail.com>
2021-02-02 13:21:21 -0800

    Updated TwentyOne, Added AssignmentStep314
    updated TwentyOne, added Assignment Step 314

---
+++ Basic_C#_Programs/AssignmentStep314/AssignmentStep314/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentStep314
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // simulate an exception
+                // throw new Exception(); 
+                int userAge = 0;
+                
+                // verifies userAge is an integer & verifies that it's a number between 1 and 120
+                while (userAge < 1 || userAge > 120)
+                {
+                    Console.WriteLine("How old are you? ");
+
+                    //try here will throw an exception if they enter a non integer or a number not between 1 and 120
+                    try
+                    {
+                        userAge = int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Only enter digits between 1 and 120.\n");
+                        continue;
+                    }
+
+                    if (userAge < 1 || userAge > 120)
+                    {
+                        Console.WriteLine("Only enter digits between 1 and 120.\n");
+                    }
+                }
+
+                // makes userAge a negative number
+                userAge = userAge * -1;
+                
+                Console.WriteLine("Have you already had your birthday for 2021? ");
+
+                // if userAnswer is no, subtracts a year
+                string userAnswer = (Console.ReadLine().ToLower());
+                if (userAnswer == "no" || userAnswer == "n")
+                {
+                    userAge--;
+                }
+                // skips the if loop when answer is yes
+                int getUserBirthYear = DateTime.Now.AddYears(userAge).Year;
+                Console.WriteLine("Your birth year is " + getUserBirthYear + ".");
+
+                Console.ReadLine();
+            }
+            // General message exception
+            catch (Exception)
+            {
+                Console.WriteLine("An error has occurred.  Contact your system administrator.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -17,10 +17,16 @@ namespace TwentyOne // namespace: way of organizing your code
 
             Console.WriteLine("Welcome to the {0}.  What is your name?", casinoName);
             string playerName = Console.ReadLine();
-            
-            Console.WriteLine("How much money are you playing with today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
-            
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("How much money are you playing with today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer)Console.WriteLine("Please enter digits only with no decimals.");                
+            }
+                        
             Console.WriteLine("Hello, {0}.  Would you like to join a game of 21 right now?", playerName);
             string answer = Console.ReadLine().ToLower();
             
@@ -37,7 +43,22 @@ namespace TwentyOne // namespace: way of organizing your code
                 player.isActivelyPlaying = true;
                 while(player.isActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException)
+                    {
+                        Console.WriteLine("Security! Kick this person out!");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch(Exception)
+                    {
+                        Console.WriteLine("An error occurred.  Please contact your system administrator.");
+                        Console.ReadLine();
+                        return;
+                    }
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing!");
