00c04c386c35bb89e9ad32a1a1c4819f3b8a3030
emmorris93 <maudi.morris@gmail.com>
2021-02-01 16:57:50 -0800

    Added AssignmentStep310
    added assignment step 310

---
+++ Basic_C#_Programs/AssignmentStep310/AssignmentStep310/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//ASSIGNMENT
+//Perform these actions and create a console app that includes the following:
+//1. Create a const variable.
+//2. Create a variable using the keyword "var.”
+//3. Chain two constructors together.
+
+namespace AssignmentStep310
+{
+    public class Program
+    {
+        const string GameName = "Welcome to Ixol's Tomb";
+
+        static Program()
+        {
+            Console.WriteLine(GameName);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nEnter your character name: ");
+            var player = new Player(Console.ReadLine());
+            Console.WriteLine("\nPlayer ID: " + player.Id.ToString());
+            Console.WriteLine("Character Name: " + player.Name);
+            Console.ReadLine();
+        }
+    }
+
+    public class Player
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; set; }
+
+        public Player()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        public Player(string name) : this()
+        {
+            this.Name = name;
+        }
+    }
+}
