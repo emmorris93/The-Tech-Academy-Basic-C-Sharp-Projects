52e708723b2f2e60318f0196299cbadb21bdf3e1
emmorris93 <maudi.morris@gmail.com>
2021-02-12 15:12:07 -0800

    Added CarInsurance
    added CarInsurance

---
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using CarInsurance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var adminVm = new List<AdminViewModel>();
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var insurees = db.Insurees.Where(x => x.Quote > 0).ToList();
+                
+                foreach(var insuree in insurees)
+                {
+                    var thisAdminVm = new AdminViewModel
+                    {
+                        Quote = insuree.Quote,
+                        FirstName = insuree.FirstName,
+                        Lastname = insuree.LastName,
+                        EmailAddress = insuree.EmailAddress
+                    };
+                    adminVm.Add(thisAdminVm);
+                }
+            }
+            return View(adminVm);
+        }
+    }
+}
