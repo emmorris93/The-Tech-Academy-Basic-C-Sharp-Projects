d432edfc45041a4c0d68d6029271a9cf43a0bbcc
emmorris93 <maudi.morris@gmail.com>
2021-02-14 14:38:02 -0800

    Added ChallengeStep450
    added ChallengeStep450

---
+++ Basic_C#_Programs/ChallengeStep450/ChallengeStep450/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using ChallengeStep450.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengeStep450.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var serverTime = DateTime.Now;
+            
+            return View(serverTime);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
